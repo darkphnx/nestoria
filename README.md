f8725bd6d76f1fe1bbc032a812c3ccedaa36189e
Dan Wentworth <dan.wentworth@gmail.com>
2011-01-09 16:52:29 +0000

    Documentation and fixes

---
+++ README.md
@@ -0,0 +1,76 @@
+Ruby Nestoria API Library
+=========================
+
+This is a simple library designed to facilitate the usage of the Nestoria API. The library supports all current Nestoria services, including:
+
+  * nestoria.com.au
+  * nestoria.com.br
+  * nestoria.de
+  * nestoria.es
+  * nestoria.fr
+  * nestoria.in
+  * nestoria.it
+  * nestoria.co.uk
+
+Usage
+=====
+
+Initialize
+----------
+
+To initialize the library call new with the TLD of Nestoria service you wish to use.
+
+    require 'nestoria/api'
+    Nestoria::Api.new(:uk)
+
+Echo
+----
+
+The echo command simply sends back any data you give it along with Nestoria's response headers:
+
+    Nestoria::Api.new(:uk).echo :foo => "bar"
+    # {"created_unix"=>1294588256, "encoding"=>"json", "action"=>"echo", "version"=>"1.19", "status_text"=>"OK", "status_code"=>"200", "foo"=>"bar", "sort"=>"nestoria_rank", "created_http"=>"Sun, 09 Jan 2011 15:50:56 GMT"}
+
+Search
+------
+
+To search Nestoria's property listings call Nestoria::Api#search along with a hash of parameters specifying the search values. The keys are the same as those listed on the Nestoria API site here: [http://www.nestoria.co.uk/help/api-search-listings](http://www.nestoria.co.uk/help/api-search-listings)
+
+keywords and keywords\_exclude take an Array object instead of a comma separated list. updated\_min can take a Time object instead of a UNIX timestamp.
+
+    Nestoria::Api.new(:uk).search :listing_type => "buy", :place_name => "Wimborne", :keywords => ["off_street_parking", "unfurnished"]
+    
+You should receive a hash containing all the values in the Nestoria response, further details can be found on the link above.
+
+Keywords
+--------
+
+For a list of valid keywords that can be used with the keywords and keywords\_exclude parameter in the search call Nestoria::Api#keywords. This will give you a set of keys and friendly names.
+
+    Nestoria::Api.new(:uk).keywords
+    # {"terrace"=>"Terrace", "studio"=>"Studio", "refurbished"=>"Refurbished", "patio"=>"Patio" ...
+
+Metadata/Average House Prices
+-----------------------------
+
+There's also support for fetching the average house price information on an area, just use Nestoria::Api#metadata with a set of location parameters.
+
+    Nestoria::Api.new(:uk).metadata :south_west => [50.965, -5.504], :north_east => [50.156, -5.136]
+
+Contributing to Nestoria API Library
+====================================
+ 
+* Check out the latest master to make sure the feature hasn't been implemented or the bug hasn't been fixed yet
+* Check out the issue tracker to make sure someone already hasn't requested it and/or contributed it
+* Fork the project
+* Start a feature/bugfix branch
+* Commit and push until you are happy with your contribution
+* Make sure to add tests for it. This is important so I don't break it in a future version unintentionally.
+* Please try not to mess with the Rakefile, version, or history. If you want to have your own version, or is otherwise necessary, that is fine, but please isolate to its own commit so I can cherry-pick around it.
+
+Copyright
+=========
+
+Copyright (c) 2011 Dan Wentworth. See LICENSE.txt for
+further details.
+
---
+++ lib/nestoria/api.rb
@@ -0,0 +1,123 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
+# Easy url encoding of hashes
+class Hash
+  def to_query
+    self.map{|k,v| "#{k}=#{v}"}.join("&")
+  end
+end
+
+module Nestoria
+  
+  class BadLocation < StandardError; end
+  class InvalidRequest < StandardError; end
+  class InternalError < StandardError; end
+  class InvalidVersion < StandardError; end
+  
+  class Api
+    
+    API_VERSION = 1.19
+    
+    # Valid search parameters - See http://www.nestoria.co.uk/help/api-search-listings for key/value details
+    LOCATION_KEYS = [ :place_name, :south_west, :north_east, :centre_point, :radius ]
+    SEARCH_KEYS   = [ :guid, :listing_type, :property_type, :min_price, :max_price, 
+      :bedroom_min, :bedroom_max, :room_min, :room_max, :bathroom_min, :bathroom_max, 
+      :size_min, :size_max, :keywords, :keywords_exclude, :has_photo, :updated_min, 
+      :number_of_results, :page, :sort ] + LOCATION_KEYS
+    
+    def initialize(country)
+      @country = country
+    end
+    
+    # Search nestoria property listings - See http://www.nestoria.co.uk/help/api-search-listings
+    def search(params)        
+      invalid_keys = params.keys - SEARCH_KEYS
+      raise InvalidRequest.new "Invalid keys: #{invalid_keys.join(", ")}" unless invalid_keys.empty?
+      
+      # Convert arrays into CSVs
+      [:keywords, :keywords_exclude].each do |key|
+        params[key] = params[key].join(",") unless params[key].nil?
+      end
+      
+      # Convert any Time/DateTime objects to UNIX time integers
+      params[:updated_min] = params[:updated_min].to_i unless params[:updated_min].nil?
+      
+      process_location! params
+      
+      request :search_listings, params
+    end
+    
+    # List of keywords that can be used in search - See http://www.nestoria.co.uk/help/api-keywords
+    def keywords
+      response = request :keywords
+      keywords = Hash.new
+      response["labels"].each do |label|
+        keywords[label["keyword"]] = label["content"]
+      end
+      keywords
+    end
+    
+    # Average house price for an area - See http://www.nestoria.co.uk/help/api-metadata
+    def metadata(params)
+      invalid_keys = params.keys - LOCATION_KEYS
+      raise InvalidRequest.new "Invalid keys: #{invalid_keys.join(", ")}" unless invalid_keys.empty?
+      
+      process_location! params
+      
+      request :metadata, params
+    end
+    
+    # Test URL request, returns whatever params you put in
+    def echo(params = {:foo => "bar"})
+      request :echo, params
+    end
+    
+    private
+    
+    def base_url
+      domains = {:au => "api.nestoria.com.au",
+                 :br => "api.nestoria.com.br",
+                 :de => "api.nestoria.de",
+                 :es => "api.nestoria.es",
+                 :fr => "api.nestoria.fr",
+                 :in => "api.nestoria.in",
+                 :it => "api.nestoria.it",
+                 :uk => "api.nestoria.co.uk"}
+       "http://#{domains[@country]}/api"
+    end
+    
+    def process_location!(params)
+      [:south_west, :north_east, :centre_point].each do |key|
+        params[key] = params[key].join(",") unless params[key].nil?
+      end
+    end
+    
+    def request(action, params = {})
+      url = "#{base_url}?version=#{API_VERSION}&action=#{action}&encoding=json&#{params.to_query}"
+      puts url
+      res = Net::HTTP.get_response(URI.parse(url))
+      data = JSON.parse(res.body)
+      
+      # Catch any errors returned from the API and raise an appropriate exception
+      if action.to_s == "search"
+        application_response_code = data["response"]["application_response_code"]
+        application_response_text = data["response"]["application_response_text"]
+        case application_response_code.to_i
+        when 200...299
+          raise BadLocation.new "#{application_response_code}, #{application_response_text}"
+        when 500
+          raise InternalError.new "#{application_response_code}, #{application_response_text}"
+        when 910...911
+          raise InvaidVersion.new "#{application_response_code}, #{application_response_text}"
+        when 900...999
+          raise InvalidRequest.new "#{application_response_code}, #{application_response_text}"
+        end
+      end
+      
+      data["response"]
+    end
+    
+  end
+end
